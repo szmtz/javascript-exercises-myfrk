0e27861d5455f5ca5f9620a2b2a171b01994a03e
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-23 14:30:34 +0000

    Add calculator solution (2/16 failed)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,63 @@
-const add = function() {
+// const add = function() {
 	
-};
+// };
 
-const subtract = function() {
+// const subtract = function() {
 	
-};
+// };
 
-const sum = function() {
+// const sum = function() {
 	
-};
+// };
 
-const multiply = function() {
+// const multiply = function() {
 
-};
+// };
 
-const power = function() {
+// const power = function() {
 	
+// };
+
+// const factorial = function() {
+	
+// };
+
+const add = function(a, b) {
+  return a + b;
 };
 
-const factorial = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
+
+const sum = function(numbers) {
+  return numbers.reduce((total, num) => total + num, 0);
+};
+
+const multiply = function(a, b) {
+  return a * b;
+};
+
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
+};
+
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+// // Example usage:
+// console.log("Addition:", add(5, 3)); // Outputs: 8
+// console.log("Subtraction:", subtract(10, 4)); // Outputs: 6
+// console.log("Sum:", sum([2, 3, 4, 5])); // Outputs: 14
+// console.log("Multiplication:", multiply(6, 7)); // Outputs: 42
+// console.log("Power:", power(2, 3)); // Outputs: 8
+// console.log("Factorial:", factorial(5)); // Outputs: 120
+
 
 // Do not edit below this line
 module.exports = {
