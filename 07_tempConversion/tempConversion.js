af9fcb9600b2c101562d1c18b8adaf898e4453b2
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-23 14:23:52 +0000

    Add tempConversion solution (failed)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,32 @@
-const convertToCelsius = function() {
-};
+// const convertToCelsius = function() {
+// };
 
-const convertToFahrenheit = function() {
-};
+// const convertToFahrenheit = function() {
+// };
+
+function convertToCelsius(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  return celsius.toFixed(1);
+}
+
+// // Example usage:
+// const fahrenheitTemp = 68;
+// const celsiusTemp = fahrenheitToCelsius(fahrenheitTemp);
+// console.log(`${fahrenheitTemp}°F is approximately ${celsiusTemp}°C.`);
+
+
+function convertToFahrenheit(celsius) {
+  const fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit.toFixed(1);
+}
+
+// // Example usage:
+// const celsiusTemp = 20;
+// const fahrenheitTemp = celsiusToFahrenheit(celsiusTemp);
+// console.log(`${celsiusTemp}°C is approximately ${fahrenheitTemp}°F.`);
+
+
+
 
 // Do not edit below this line
 module.exports = {
