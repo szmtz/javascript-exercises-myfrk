dbd339e10b94965d02fbec6fbba2105c9a13cf55
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-23 13:57:42 +0000

    Add reverseString solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,27 @@
-const reverseString = function() {
+// const reverseString = function() {
 
-};
+// };
+
+function reverseString(input) {
+    if (typeof input !== 'string') {
+      return "Input is not a string.";
+    }
+  
+    let reversed = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+      reversed += input[i];
+    }
+  
+    return reversed;
+  }
+  
+//   // Example usage:
+//   const originalString = "Hello, World!";
+//   const reversed = reverseString(originalString);
+//   console.log(reversed); // Outputs: "!dlroW ,olleH"
+  
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
