8be1a4c10d16d6126075b033dcc521b09a186dc4
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-23 14:10:12 +0000

    Add sumAll solution (failed)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+// const sumAll = function() {
 
-};
+function sumAll(start, end) {
+    if (typeof start !== 'number' || typeof end !== 'number') {
+      return "Both arguments must be numbers.";
+    }
+  
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+      sum += i;
+    }
+  
+    return sum;
+  }
+  
+//   // Example usage:
+//   const result = sumNumbersBetween(1, 5);
+//   console.log(result); // Outputs: 15 (1 + 2 + 3 + 4 + 5)
+
 
 // Do not edit below this line
 module.exports = sumAll;
