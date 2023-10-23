3822e9cf16747d644137025b4217b57dbe96f182
saúl z mtz <97975590+szmtz@users.noreply.github.com>
2023-10-23 14:02:06 +0000

    Add removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+// const removeFromArray = function() {
 
-};
+// };
+
+function removeFromArray(array, ...argsToRemove) {
+    if (!Array.isArray(array)) {
+      throw new Error('The first argument must be an array.');
+    }
+  
+    return array.filter(item => !argsToRemove.includes(item));
+  }
+  
+//   // Example usage:
+//   const originalArray = [1, 2, 3, 4, 5];
+//   const newArray = removeFromArray(originalArray, 3, 5);
+//   console.log(newArray); // Outputs: [1, 2, 4]
+  
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
